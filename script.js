c15d3feca6021c2da1a33e0a6759dbff448ea838
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-04-29 10:50:25 -0300

    SIN TERMINAR

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-let iniciar = document.getElementById("startButton")
-let cantGame = 1
-let combination = []
+let iniciar = document.getElementById("startButton");
+let cantGame = 1;
+let combination = [];
 let instance = 0;
-botones = [document.getElementById("b0"),document.getElementById("b1"),document.getElementById("b2"),document.getElementById("b3")]
+let botones = [document.getElementById("b0"), document.getElementById("b1"), document.getElementById("b2"), document.getElementById("b3")];
 
-function random(x){
-    return Math.floor(Math.random * x)
-}
-function ableButtons(x){
-    if(x==false){
-    for(let i=0;i<4; i++ ){
-        botones[i].disabled = true
-    }
-} else if(x==true){
-    for(let i=0;i<4; i++ ){
-        botones[i].disabled = false
-    }
-}
-}
-function game(x){
-    combination.unshift(random(4))
-    console.log(combination)
-    return(combination)
+function random(x) {
+    return Math.floor(Math.random() * x);
 }
 
-function saying(){
-    for(e of combination){
+function ableButtons(x) {
+    for (let i = 0; i < 4; i++) {
+        botones[i].disabled = !x;
+    }
+}
+
+function addCombination() {
+    combination.unshift(random(4));
+    console.log(combination);
+    return combination;
+}
+
+function saying() {
+    for (let i = 0; i < combination.length; i++) {
+        let originalColor = botones[combination[i]].style.backgroundColor; 
         setTimeout(() => {
-            botones[e].style.backgroundColor = black;
-        }, 1000);
+            botones[combination[i]].style.backgroundColor = `rgba(0, 0, 0, 0.5)`; 
+            setTimeout(() => {
+                botones[combination[i]].style.backgroundColor = originalColor; 
+            }, 1000); 
+        }, i * 2000); 
     }
 }
 
-function noPlay(){
-    ableButtons(false)
-    cantGame += 1
-    game(cantGame)
-    saying()
-    instance ++
+
+function noPlay() {
+    ableButtons(false);
+    cantGame += 1;
+    addCombination();
+    saying();
+    instance++;
 }
 
-function play(){
-    ableButtons(true)
-    
+function play() {
+    ableButtons(true);
 }
 
-
-iniciar.addEventListener("click", function(){
-    while(true){
-        console.log(instance)
-        noPlay()
-        console.log(instance)
-        play()
-    }
-
-}
-)
+iniciar.addEventListener("click", function () {
+    console.log(instance);
+    noPlay();
+    console.log(instance);
+    play();
+});
